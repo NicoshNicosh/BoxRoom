0b923fadd47a37edafc955e69fcd13af6e3d8c37
MHDante <hello@mhdante.com>
2021-05-16 21:06:16 -0400

    Last minute

--- BoxRoomUnity/Assets/TitleScreen.cs
+++ BoxRoomUnity/Assets/TitleScreen.cs
@@ -8,4 +8,11 @@ public class TitleScreen : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void OnRestart()
+    {
+        if(MusicManager.Instance) Destroy(MusicManager.Instance.gameObject);
+        if(SpawnableManager.Instance) Destroy(SpawnableManager.Instance.gameObject);
+        SceneManager.LoadScene(0);
+    }
 }
